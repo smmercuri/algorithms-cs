f24b3fd9bdd03ba17ccc3c1866416afff338c1b7
smmercuri <47568553+smmercuri@users.noreply.github.com>
2020-09-21 17:21:15 +0100

    Add files via upload

---
+++ collinear/Point.java
@@ -0,0 +1,127 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+    /**
+     * Initializes a new point.
+     *
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    /**
+     * Draws the line segment between this point and the specified point to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point. Formally, if the two points are
+     * (x0, y0) and (x1, y1), then the slope is (y1 - y0) / (x1 - x0). For completeness, the slope
+     * is defined to be +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical; and Double.NEGATIVE_INFINITY if
+     * (x0, y0) and (x1, y1) are equal.
+     *
+     * @param that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if (this.compareTo(that) == 0) return Double.NEGATIVE_INFINITY;
+        else if (this.x == that.x) return Double.POSITIVE_INFINITY;
+        else if (this.y == that.y) return 0.0;
+        else return ((double) that.y - this.y) / (that.x - this.x);
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate. Formally, the invoking
+     * point (x0, y0) is less than the argument point (x1, y1) if and only if either y0 < y1 or if
+     * y0 = y1 and x0 < x1.
+     *
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument point (x0 = x1 and y0 =
+     * y1); a negative integer if this point is less than the argument point; and a positive integer
+     * if this point is greater than the argument point
+     */
+    public int compareTo(Point that) {
+        int yComp = this.y > that.y ? 1 : -1;
+        int xComp = this.x > that.x ? 1 : -1;
+        if (this.y != that.y) return yComp;
+        else if (this.x != that.x) return xComp;
+        else return 0;
+    }
+
+    /**
+     * Compares two points by the slope they make with this point. The slope is defined as in the
+     * slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        Comparator<Point> SLOPE_ORDER = new SlopeOrder();
+        return SLOPE_ORDER;
+    }
+
+    private class SlopeOrder implements Comparator<Point> {
+        public int compare(Point a, Point b) {
+            double aSlope = slopeTo(a);
+            double bSlope = slopeTo(b);
+            int less = aSlope > bSlope ? 1 : -1;
+            if (aSlope == bSlope) return 0;
+            return less;
+        }
+    }
+
+
+    /**
+     * Returns a string representation of this point. This method is provide for debugging; your
+     * program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(1, 2);
+        StdOut.println(p1.slopeTo(p2));
+        StdOut.println(p1.compareTo(p2));
+    }
+}
